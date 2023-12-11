118fec4954102ae7dfe5198742a4e323c122b8fe
TsunaHalo2001 <145141553+TsunaHalo2001@users.noreply.github.com>
2023-12-11 13:10:24 -0500

    Añadido backend de registro e inicio de sesion

--- Proyecto.py
+++ Proyecto.py
@@ -1,7 +1,9 @@
 import tkinter
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, messagebox
 from PIL import Image, ImageTk
+import numpy as np
+import hashlib
 
 import sv_ttk
 
@@ -15,6 +17,21 @@ bgcolor = "#DCDCDC"
 mydescription = "\nYa sea que busques un almuerzo casual con\namigos, una cena romántica o un evento\nespecial, nuestro restaurante\npromete una experiencia que cautivará tu\npaladar y creará recuerdos duraderos.\n"
 bwregist = 37 / 1366
 
+txtloginlist = open("txt/tloginlist.txt", "r")
+loginlist = txtloginlist.read()
+txtloginlist.close()
+loginlist = loginlist.replace(" ", "")
+loginlist = loginlist.replace("'", "")
+loginlist = loginlist.replace("],[", "/")
+loginlist = loginlist.replace("]]", "")
+loginlist = loginlist.replace("[[", "")
+lloginlist = loginlist.split("/")
+
+for i in range(len(lloginlist)):
+    lloginlist[i] = lloginlist[i].split(",")
+
+emaildb = ["@correounivalle.edu.co","@gmail.com", "@hotmail.com", "@outlook.com", "@yahoo.com", "@icloud.com", "@live.com", "@msn.com", "@aol.com", "@yandex.com", "@protonmail.com", "@zoho.com", "@gmx.com", "@mail.com", "@yopmail.com", "@tutanota.com", "@mail.ru", "@gmx.us", "@gmx.de", "@gmx.fr", "@gmx.at", "@gmx.ch", "@gmx.net", "@gmx.co.uk", "@gmx.com.mx", "@gmx.es", "@gmx.eu", "@gmx.it", "@gmx.com.br", "@gmx.com.ar", "@gmx.com.co", "@gmx.com.ve", "@gmx.com.pe", "@gmx.com.ec", "@gmx.com.bo", "@gmx.com.py", "@gmx.com.uy", "@gmx.com.pa", "@gmx.com.do", "@gmx.com.gt", "@gmx.com.sv", "@gmx.com.hn", "@gmx.com.ni", "@gmx.com.cr", "@gmx.com.cu", "@gmx.com.pr", "@gmx.com.jm", "@gmx.com.bb", "@gmx.com.ag", "@gmx.com.dm", "@gmx.com.vc", "@gmx.com.lc", "@gmx.com.gy", "@gmx.com.sr", "@gmx.com.bo", "@gmx.com.py", "@gmx.com.uy", "@gmx.com.ar", "@gmx.com.co", "@gmx.com.ve", "@gmx.com.pe", "@gmx.com.ec", "@gmx.com.bo", "@gmx.com.py", "@gmx.com.uy", "@gmx.com.pa", "@gmx.com.do", "@gmx.com.gt", "@gmx.com.sv", "@gmx.com.hn", "@gmx.com.ni", "@gmx.com.cr", "@gmx.com.cu", "@gmx.com.pr", "@gmx.com.jm", "@gmx.com.bb", "@gmx.com.ag", "@gmx.com.dm", "@gmx.com.vc", "@gmx.com.lc", "@gmx.com.gy", "@gmx.com.sr", "@gmx.com.bo", "@gmx.com.py", "@gmx.com.uy"]
+
 def back():
     global main, fmainmenu, finicio, flogin, fregist, pstate
 
@@ -25,7 +42,7 @@ def back():
         fmainmenu.grid(row = 0, column = 0)
 
 def regist():
-    global main, fmainmenu, finicio, fregist, pstate
+    global main, fmainmenu, finicio, flogin, fregist, pstate
 
     pstate = 1
 
@@ -34,12 +51,13 @@ def regist():
 
     main.add(finicio, text = "Inicio")
     main.add(fregist, text = "Registrarse")
+    main.add(flogin, text = "Iniciar sesion")
     main.select(fregist)
     main.bind("<<NotebookTabChanged>>", lambda event: back())
     main.forget(flogin)
 
 def login():
-    global main, fmainmenu, finicio, fregist, pstate
+    global main, fmainmenu, finicio, flogin ,fregist, pstate
 
     pstate = 1
 
@@ -48,15 +66,87 @@ def login():
 
     main.add(finicio, text = "Inicio")
     main.add(flogin, text = "Iniciar sesion")
+    main.add(fregist, text = "Registrarse")
     main.select(flogin)
     main.bind("<<NotebookTabChanged>>", lambda event: back())
     main.forget(fregist)
 
+def welcome():
+    global main, fwelcome, pstate
+
+    pstate = 2
+
+    main.add(fwelcome, text = "Menu principal")
+    main.select(fwelcome)
+    main.bind("<<NotebookTabChanged>>", lambda event: back())
+    main.forget(flogin)
+
 def slogin():
-    global main, finicio, flogin, pstate
+    global main, finicio, flogin, pstate, lloginlist, eluser, elpassword
+
+    tloginlist = []
+
+    if elpassword.get() == "" or eluser.get() == "":
+        messagebox.showerror("Error", "No se pueden dejar campos vacios")
+
+    else:
+        try:
+            tloginlist.append(hashlib.sha256(eluser.get().lower().encode()).hexdigest())
+            tloginlist.append(hashlib.sha256(elpassword.get().encode()).hexdigest())
+
+            lloginlist.index(tloginlist)
+
+            messagebox.showinfo("Iniciado", "Sesion iniciada con exito")
+            
+            welcome()
+
+        except ValueError:
+            messagebox.showerror("Error", "Usuario o contraseña incorrectos")
+
+def sregist():
+    global main, finicio, fregist, pstate, lloginlist, eruser, erpassword, ercpassword
+
+    tloginlist = []
+
+    if erpassword.get() == "" or eruser.get() == "" or ercpassword.get() == "":
+        messagebox.showerror("Error", "No se pueden dejar campos vacios")
+
+    elif eruser.get().count("@") != 1 or eruser.get()[eruser.get().index("@"):] not in emaildb:
+        messagebox.showerror("Error", "Correo invalido")
+
+    elif erpassword.get() == ercpassword.get():
+        try:
+            erpassword.get().index(" ")
+            ercpassword.get().index(" ")
+            eruser.get().index(" ")
+            messagebox.showerror("Error", "No se pueden usar espacios en los campos")
+
+        except ValueError:
+            tloginlist.append(hashlib.sha256(eruser.get().lower().encode()).hexdigest())
+            tloginlist.append(hashlib.sha256(erpassword.get().encode()).hexdigest())
+
+            try:
+                lloginlist.index(tloginlist)
+                messagebox.showerror("Error", "El usuario ya existe")
+
+            except ValueError:
+                lloginlist.append(tloginlist)
+
+                txtloginlist = open("txt/tloginlist.txt", "w")
+                txtloginlist.write(str(lloginlist))
+                txtloginlist.close()
+
+                messagebox.showinfo("Registrado", "Usuario registrado con exito")
+                back()
+
+    else:
+        messagebox.showerror("Error", "Las contraseñas no coinciden")
 
 def run():
-    global ssize, fsize, root, ico, photo, main, fmainmenu, finicio, flogin, fregist
+    global ssize, fsize, root, ico, photo
+    global main, fmainmenu, finicio, flogin, fregist, fwelcome
+    global eluser, elpassword
+    global eruser, erpassword, ercpassword
 
     root = tkinter.Tk()
     
@@ -117,15 +207,17 @@ def run():
 
     fltext = Frame(flogin, bg = bgcolor)
 
-    euser = Entry(fltext, font = (ftitle, int(fsize / 2), "bold"), width = int(ssize[0] * 0.01))
-    euser.insert(0, "Usuario")
-    euser.bind("<FocusIn>", lambda event: euser.delete(0, END))
+    lluser = Label(fltext, text = "Email", font = (ftitle, int(fsize / 2), "bold"), bg = bgcolor)
 
-    epassword = Entry(fltext, show = "*",font = (ftitle, int(fsize / 2), "bold"), width = int(ssize[0] * 0.01))
-    epassword.insert(0, "Contraseña")
-    epassword.bind("<FocusIn>", lambda event: epassword.delete(0, END))
+    eluser = Entry(fltext, font = (ftitle, int(fsize / 2), "bold"), width = int(ssize[0] * 0.01))
 
-    bslogin = Button(flogin, text = "Iniciar sesion", font = (ftitle, int(fsize / 2), "bold"), height = int(ssize[1] * 0.002), width = int(ssize[0] * 0.009))
+    llpassword = Label(fltext, text = "Contraseña", font = (ftitle, int(fsize / 2), "bold"), bg = bgcolor)
+
+    elpassword = Entry(fltext, show = "*",font = (ftitle, int(fsize / 2), "bold"), width = int(ssize[0] * 0.01))
+
+    ltlogin = Label(fltext, text = " ", font = (ftitle, int(fsize / 5), "bold"), bg = bgcolor)
+
+    bslogin = Button(flogin, text = "Iniciar sesion", command = slogin, font = (ftitle, int(fsize / 2), "bold"), height = int(ssize[1] * 0.002), width = int(ssize[0] * 0.009))
 
     ftrlspace = Frame(flogin, width = ssize[0] * 0.39, height = ssize[0] * 0.05, bg = bgcolor)
 
@@ -135,12 +227,38 @@ def run():
 
     fregist = Frame(main, bg = bgcolor)
 
-    ftlrspace = Frame(fregist, width = ssize[0] * 0.05, height = ssize[0] * 0.05, bg = bgcolor)
+    ftlrspace = Frame(fregist, width = ssize[0] * 0.39, height = ssize[0] * 0.05, bg = bgcolor)
 
     lregisttitle = Label(fregist, text = mrtitle, font = (ftitle, fsize, "bold"), fg = fctitle, bg = bgcolor)
 
     iregisttitle = ImageTk.PhotoImage(ico.resize((int(ssize[0] * 0.1), int(ssize[0] * 0.1 * logoar))))
     liregisttitle = tkinter.Label(fregist, image = ilogintitle, bg = bgcolor)
+
+    frtext = Frame(fregist, bg = bgcolor)
+
+    lruser = Label(frtext, text = "Email", font = (ftitle, int(fsize / 2), "bold"), bg = bgcolor)
+
+    eruser = Entry(frtext, font = (ftitle, int(fsize / 2), "bold"), width = int(ssize[0] * 0.01))
+
+    lrpassword = Label(frtext, text = "Contraseña", font = (ftitle, int(fsize / 2), "bold"), bg = bgcolor)
+
+    erpassword = Entry(frtext, show = "*",font = (ftitle, int(fsize / 2), "bold"), width = int(ssize[0] * 0.01))
+
+    lrcpassword = Label(frtext, text = "Confirmar contraseña", font = (ftitle, int(fsize / 2), "bold"), bg = bgcolor)
+
+    ercpassword = Entry(frtext, show = "*",font = (ftitle, int(fsize / 2), "bold"), width = int(ssize[0] * 0.01))
+
+    ltregist = Label(frtext, text = " ", font = (ftitle, int(fsize / 5), "bold"), bg = bgcolor)
+
+    bsregist = Button(fregist, text = "Registrarse", command = sregist, font = (ftitle, int(fsize / 2), "bold"), height = int(ssize[1] * 0.002), width = int(ssize[0] * 0.009))
+
+    ftrrspace = Frame(fregist, width = ssize[0] * 0.39, height = ssize[0] * 0.05, bg = bgcolor)
+
+    fclrspace = Frame(fregist, width = ssize[0] * 0.05, height = ssize[0] * 0.05, bg = bgcolor)
+
+    #BIENVENIDO
+
+    fwelcome = Frame(main, bg = bgcolor)
 
     #Posicionamiento de Frames
 
@@ -168,16 +286,30 @@ def run():
     llogintitle.grid(row = 1, column = 1)
     lilogintitle.grid(row = 2, column = 1)
     fltext.grid(row = 4, column = 1)
-    euser.grid(row = 4, column = 1)
-    epassword.grid(row = 5, column = 1)
-    bslogin.grid(row = 6, column = 1)
+    lluser.grid(row = 4, column = 1)
+    eluser.grid(row = 5, column = 1)
+    llpassword.grid(row = 6, column = 1)
+    elpassword.grid(row = 7, column = 1)
+    ltlogin.grid(row = 8, column = 1)
+    bslogin.grid(row = 9, column = 1)
     ftrlspace.grid(row = 0, column = 2)
 
     #Posicionamiento del registro
 
     ftlrspace.grid(row = 0, column = 0)
+    fclrspace.grid(row = 3, column = 0)
     lregisttitle.grid(row = 1, column = 1)
     liregisttitle.grid(row = 2, column = 1)
+    frtext.grid(row = 4, column = 1)
+    lruser.grid(row = 4, column = 1)
+    eruser.grid(row = 5, column = 1)
+    lrpassword.grid(row = 6, column = 1)
+    erpassword.grid(row = 7, column = 1)
+    lrcpassword.grid(row = 8, column = 1)
+    ercpassword.grid(row = 9, column = 1)
+    ltregist.grid(row = 10, column = 1)
+    bsregist.grid(row = 11, column = 1)
+    ftrrspace.grid(row = 0, column = 2)
 
     root.mainloop()
 
